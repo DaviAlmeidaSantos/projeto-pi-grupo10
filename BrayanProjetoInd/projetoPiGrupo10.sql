eaf2e731787c0c855876b1bbad83843f065191df
brayanlainesc <brayan.carvalho@sptech.school>
2026-09-06 20:03:48 -0300

    Corrigindo atributos do Banco de Dados do Brayan

--- BrayanProjetoInd/projetoPiGrupo10.sql
+++ BrayanProjetoInd/projetoPiGrupo10.sql
@@ -8,54 +8,54 @@ USE insumais;
 
 CREATE TABLE cliente(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(200) NOT NULL,
-    cnpj CHAR(14) NOT NULL UNIQUE,
-    senha VARCHAR(30) NOT NULL
+	nome VARCHAR(200) NOT NULL,
+	cnpj CHAR(14) NOT NULL UNIQUE,
+	senha VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE canavial(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    
-    -- Ligação com cliente
-    cliente_nome INT
+	
+	-- Ligação com cliente
+	cliente_nome VARCHAR(200)
 );
 
 CREATE TABLE insumo(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	tipo VARCHAR(100),
-    preco_ton DECIMAL(10,2),
-    fabricante VARCHAR(100)
+	preco_ton DECIMAL(10,2),
+	fabricante VARCHAR(100)
 );
-    
+	
 CREATE TABLE amostra(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    insumo_qtd DECIMAL(6,2),
-    dt_plantacao DATETIME,
-    
-    -- Ligação com canavial
-    canavial_id VARCHAR(200),
-    
-    -- Ligação com insumo
-    insumo_tipo VARCHAR(100)
+	insumo_qtd DECIMAL(8,2),
+	dt_plantacao DATETIME,
+	
+	-- Ligação com canavial
+	canavial_id INT,
+	
+	-- Ligação com insumo
+	insumo_tipo VARCHAR(100)
 );
 
 CREATE TABLE cana(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	altura DECIMAL(6,2),
-    raio DECIMAL(5,2),
-    
-    -- Ligação com amostra
-    amostra_insumo_tipo VARCHAR(100),
-    amostra_insumo_qtd DECIMAL(6,2)
+	raio DECIMAL(5,2),
+	
+	-- Ligação com amostra
+	amostra_insumo_tipo VARCHAR(100),
+	amostra_insumo_qtd DECIMAL(8,2)
 );
 
 CREATE TABLE sensor(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	cana_id INT,
-    orientacao CHAR(1),
-    captura DECIMAL(6,2),
-    CONSTRAINT chkOrientacao CHECK(orientacao IN('V', 'H')),
-    
-    -- Ligação com cana
-    cana_id INT
+	orientacao CHAR(1),
+	captura DECIMAL(6,2),
+	CONSTRAINT chkOrientacao CHECK(orientacao IN('V', 'H')),
+	
+	-- Ligação com cana
+	cana_id INT
 );
